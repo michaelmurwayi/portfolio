948b4aad8cd8117382e01b90cb03ec2c75d1a1f7
michaelmurwayi <mikemurwayi7@gmail.com>
2021-04-11 17:50:08 +0300

    page header and intro

---
+++ src/components/Header.vue
@@ -0,0 +1,57 @@
+<template>
+<v-app>
+  <div class="header">
+    <div class="container">
+      <h1 class="name"> Michael Murwayi_ </h1>
+      <h5>Software Developer, Cyber Security</h5>
+      <div class="row">
+
+      <div class="label" style="margin-left:15px; margin-top:30px; color:black; font-size:2.5vh;"> 
+      <p><strong>Age:</strong></p> 
+      <p><strong>Phone:</strong></p>
+      <p><strong>Address:</strong></p>
+    </div>
+    <div class="details" style="margin-left:45px; margin-top:30px; font-size:2.5vh;">
+      <p><strong>23</strong></p> 
+      <p><strong>0746256084</strong></p>
+      <p><strong>Nairobi Kenya</strong></p>
+    </div>
+  </div>
+  </div>
+  </div>  
+</v-app>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class HelloWorld extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+.header{
+  background-image:linear-gradient(to right, rgba(0, 0, 252, 0.73), rgba(106, 90, 205, 0.73)), url('../assets/me.jpeg');
+  background-size: cover;
+  height: 60vh;
+  width: 100vw;
+  position: absolute;
+  top: 0px;
+  left: 0px;
+}
+.container{
+  position: absolute;
+  top:20vh;
+  left: 40vw;
+  width: 500px;
+  color: white;
+}
+.label{
+  color:white;
+
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,18 @@
+import '@fortawesome/fontawesome-free/css/all.min.css'
+import 'bootstrap-css-only/css/bootstrap.min.css'
+import 'mdbvue/lib/css/mdb.min.css'
 import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
